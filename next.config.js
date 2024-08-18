cf2dbee22bf488e2444f020004ad7c1f9c0ef9a1
datleXx <129921351+datleXx@users.noreply.github.com>
2024-08-19 06:41:54 +1000

    Fix ( Dat ) : Deployment runout bug fixed

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  staticPageGenerationTimeout: 2000,
   images: {
     domains: ["media.licdn.com", "lh3.googleusercontent.com"],
   },
